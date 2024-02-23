d85193be8dd7a64fe4a4cdff4f0640dc6c35cb1a
Denis Nedkov <113765690+DenisBG312@users.noreply.github.com>
2024-02-23 22:43:30 +0200

    Add project files.

---
+++ AuctionsMain/Data/Services/IListingsService.cs
@@ -0,0 +1,11 @@
+﻿using AuctionsMain.Models;
+
+namespace AuctionsMain.Data.Services
+{
+    public interface IListingsService
+    {
+        IQueryable<Listing> GetAll();
+        Task Add(Listing listing);
+        Task<Listing> GetById(int? id);
+    }
+}
